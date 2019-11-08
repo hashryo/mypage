3fe7711733d5cc07366b9610aea966f1f175ce8e
hashryo <ryo_hashimoto1108@icloud.com>
2019-11-08 10:55:16 +0900

    designupdate

--- javascripts/design.js
+++ javascripts/design.js
@@ -8,31 +8,25 @@ document.addEventListener("DOMContentLoaded", function(){
 // 　パララクス
 document.addEventListener('scroll', function(){
   var y = window.pageYOffset ;
-  if (y > 1100){
-    document.querySelector('.element-1').style.top = `${100+y/3}px`;
-    document.querySelector('.element-2').style.top = `${100+y/4}px`;
-    document.querySelector('.element-3').style.top = `${100+y/3}px`;
-    document.querySelector('.element-4').style.top = `${100+y/5}px`;
-    document.querySelector('.element-5').style.top = `${100+y/3}px`;
-    document.querySelector('.element-6').style.top = `${100+y/4}px`;
-    document.querySelector('.element-7').style.top = `${100+y/3}px`;
-  } 
   document.querySelector('.block-1').style.backgroundPosition = `0 ${y}px`;
-  if (y > 200){
-    document.querySelector('.block-2').style.backgroundPosition = `0 ${y-200}px`;
+  if (y > 340){
+    document.querySelector('.block-2').style.backgroundPosition = `0 ${y-340}px`;
   } else{
     document.querySelector('.block-2').style.backgroundPosition = `0 0`;
   }
-  if (y > 400){
-    document.querySelector('.block-3').style.backgroundPosition = `0 ${(y-400)*2}px`;
+  if (y > 680){
+    document.querySelector('.block-3').style.backgroundPosition = `0 ${(y-680)*2}px`;
   } else{
     document.querySelector('.block-3').style.backgroundPosition = `0 0`;
   }
-  if (y > 400 ){
-    document.querySelector('.block-4').style.opacity = `${(y-400)/200}`;
-    document.querySelector('.block-4').style.top = `320px`;
+  if (y > 680){
+    document.querySelector('.block-4').style.opacity = `${(y-800)/340}`;
+    document.querySelector('.block-4').style.top = `430px`;
   } else{
     document.querySelector('.block-4').style.opacity = `0`;
+  }
+  if (y > 1000 ){
+    document.querySelector('.block-4').style.display = `none`;
   }
 });
   
